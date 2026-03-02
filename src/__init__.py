35b40e4e1cf4e6ce041cdb3c0fd2dc75f01ca7c6
artem_malanin <malanin.aa@phystech.edu>
2026-03-02 14:04:46 +0300

    feat: update package init with all exports
    - Add Variable classes to exports
    - Add Instrument classes to exports
    - Add Constant to exports
    - Update __all__ list

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,17 @@ Core data model for Experiments project.
 """
 
 from .experiment import Experiment
+from .variable import Variable, VariableMeasured, VariableCalculated
+from .instrument import Instrument, InstrumentAbsolute, InstrumentRelative
+from .constant import Constant
 
-__all__ = ['Experiment']
+__all__ = [
+    'Experiment',
+    'Variable',
+    'VariableMeasured',
+    'VariableCalculated',
+    'Instrument',
+    'InstrumentAbsolute',
+    'InstrumentRelative',
+    'Constant',
+]
