b8a72bf7fa741b624ec0e3ec7c1568ab17654f2e
artem_malanin <malanin.aa@phystech.edu>
2026-03-16 02:06:26 +0300

    Export table CSV APIs and add format contract example

--- src/serializers/__init__.py
+++ src/serializers/__init__.py
@@ -4,5 +4,12 @@ Serializers for Experiment data persistence.
 
 from .json_serializer import ExperimentSerializer
 from .csv_handler import CSVHandler
+from .table_data import TableData
+from .csv_table_adapter import CSVTableAdapter
 
-__all__ = ['ExperimentSerializer', 'CSVHandler']
+__all__ = [
+	"ExperimentSerializer",
+	"CSVHandler",
+	"TableData",
+	"CSVTableAdapter",
+]
