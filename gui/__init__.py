7e410da65a900217a5379306499050c56eea1a2d
artem_malanin <malanin.aa@phystech.edu>
2026-03-16 12:59:31 +0300

    refactor(gui): modular gui package and instruments table model

---
+++ gui/__init__.py
@@ -0,0 +1,4 @@
+from .views.main_window import MainWindow
+from .controllers.main_controller import MainController
+
+__all__ = ["MainWindow", "MainController"]
---
+++ gui/views/main_window.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QMainWindow, QWidget
+
+from ui_mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget = None) -> None:
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
