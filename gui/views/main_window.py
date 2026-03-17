bcc3e99bec8c050a7df2dd38068942a1c2a4b78a
artem_malanin <malanin.aa@phystech.edu>
2026-03-18 01:31:47 +0300

    Добавлен пробный график через QtCharts (QChartView) в интерфейс и пример инициализации графика в main_window.py

--- gui/views/main_window.py
+++ gui/views/main_window.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QMainWindow, QWidget
 
 from ui_mainwindow import Ui_MainWindow
+from PySide6.QtCharts import QChart, QChartView, QLineSeries
+from PySide6.QtGui import QPainter
 
 
 class MainWindow(QMainWindow):
@@ -8,3 +10,22 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.init_plot()
+
+    def init_plot(self):
+        # Пример: создаём простой график (линейный график с тестовыми данными)
+        series = QLineSeries()
+        series.append(0, 0)
+        series.append(1, 1)
+        series.append(2, 0.5)
+        series.append(3, 1.5)
+        series.append(4, 1)
+
+        chart = QChart()
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+        chart.setTitle("Пробный график (QtCharts)")
+
+        self.ui.plotChartView.setChart(chart)
+        self.ui.plotChartView.setRenderHint(QPainter.Antialiasing)
+        # Для дальнейшей работы: замените данные на реальные экспериментальные значения
