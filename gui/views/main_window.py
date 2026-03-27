b0ca2b59ec2ea3c691906cce9a1aa341d762fefa
artem_malanin <malanin.aa@phystech.edu>
2026-03-27 18:22:11 +0300

    feat: добавить PlotManager и автопостроение графиков переменных
    - Создан PlotManager (gui/views/plot_manager.py) с поддержкой типов графиков:
      * scatter - точечный график (идемпотентный по индексу)
      * line - линейный график через точки
      * histogram - гистограмма распределения значений
      * approximation - линейная аппроксимация (линия тренда)
    
    - Очищены тестовые данные из MainWindow
    - При выборе переменной в дереве автоматически строится точечный график
    - При редактировании значений переменной график пересчитывается
    - При изменении прибора график связанной переменной обновляется
    - При выборе константы/прибора график очищается
    
    Это закладывает фундамент для визуализации распределений и аппроксимаций.

--- gui/views/main_window.py
+++ gui/views/main_window.py
@@ -1,8 +1,7 @@
 from ui_mainwindow import Ui_MainWindow
 
 from PySide6.QtWidgets import QMainWindow, QWidget, QHeaderView
-from PySide6.QtCharts import QChart, QChartView, QLineSeries
-from PySide6.QtGui import QPainter
+from .plot_manager import PlotManager
 
 
 class MainWindow(QMainWindow):
@@ -19,22 +18,5 @@ class MainWindow(QMainWindow):
         self.ui.tableValues.verticalHeader().setVisible(False)
         self.ui.tableInstruments.verticalHeader().setVisible(False)
 
-        self.init_plot()
-
-    def init_plot(self):
-        # Пример: создаём простой график (линейный график с тестовыми данными)
-        series = QLineSeries()
-        series.append(0, 0)
-        series.append(1, 1)
-        series.append(2, 0.5)
-        series.append(3, 1.5)
-        series.append(4, 1)
-
-        chart = QChart()
-        chart.addSeries(series)
-        chart.createDefaultAxes()
-        chart.setTitle("Пробный график (QtCharts)")
-
-        self.ui.plotChartView.setChart(chart)
-        self.ui.plotChartView.setRenderHint(QPainter.Antialiasing)
-        # Для дальнейшей работы: замените данные на реальные экспериментальные значения
+        # Инициализируем менеджер графиков
+        self.plot_manager = PlotManager(self.ui.plotChartView)
